c66172688f6462426a78226ca5747d7dfbeea770
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-10-05 11:26:35 +0900

    Fix: fix Role 1. matchingCount 추가 2. 순서 오류 변경

--- src/main/java/com/resengkor/management/domain/user/entity/Role.java
+++ src/main/java/com/resengkor/management/domain/user/entity/Role.java
@@ -1,19 +1,25 @@
 package com.resengkor.management.domain.user.entity;
 
 public enum Role {
-    ROLE_GUEST("ROLE_GUEST"),
-    ROLE_MANAGER("ROLE_MANAGER"),
-    ROLE_DISTRIBUTOR("ROLE_DISTRIBUTOR"),
-    ROLE_AGENCY("ROLE_AGENCY"),
-    ROLE_CUSTOMER("ROLE_CUSTOMER");
+    ROLE_GUEST("ROLE_GUEST", 1),
+    ROLE_CUSTOMER("ROLE_CUSTOMER", 2),
+    ROLE_AGENCY("ROLE_AGENCY", 3),
+    ROLE_DISTRIBUTOR("ROLE_DISTRIBUTOR", 4),
+    ROLE_MANAGER("ROLE_MANAGER", 5);
 
-    String role;
+    private String role;
+    private int rank;
 
-    Role(String role){
+    Role(String role, int rank) {
         this.role = role;
+        this.rank = rank;
     }
 
-    public String value(){
+    public String getRole() {
         return role;
+    }
+
+    public int getRank() {
+        return rank;
     }
 }
