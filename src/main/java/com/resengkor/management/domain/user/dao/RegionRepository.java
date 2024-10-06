d8a03953b2473f7401a76928acffe7e2a98d3ce2
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-10-07 01:58:47 +0900

    feat: Region Repository 추가

---
+++ src/main/java/com/resengkor/management/domain/user/dao/RegionRepository.java
@@ -0,0 +1,13 @@
+package com.resengkor.management.domain.user.dao;
+
+
+import com.resengkor.management.domain.user.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RegionRepository extends JpaRepository<Region, Long> {
+    Optional<Region> findByRegionName(String regionName);
+}
