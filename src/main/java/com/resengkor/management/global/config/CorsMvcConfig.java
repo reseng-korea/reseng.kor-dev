b6a8d06ddc10700b26d3838188a6508a8a326e10
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-10-11 19:27:46 +0900

    Fix:CorsMvcConfig 추가

---
+++ src/main/java/com/resengkor/management/global/config/CorsMvcConfig.java
@@ -0,0 +1,15 @@
+package com.resengkor.management.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .exposedHeaders("Set-Cookie") //노출할 헤더값은 쿠키 헤더
+                .allowedOrigins("http://localhost:3000"); //웹앱이 동작할 서버 주소
+    }
+}
