587967bd62dabbc3d51bd39fe785aa27dbb3c9e7
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-10-11 19:33:53 +0900

    Fix:OAuth 구현 1. CustomOAuth2SuccessHandler 구현 2. CustomOAuth2User 구현 3. CustomOAuth2UserService 구현 4. GoogleResponse/KakaoResponse 구현 5. OAuth2Controller 구현 6. OAuth2JwtHeaderService 구현 7. OAuth2Response 구현 8. OAuth2UserDto 구현

---
+++ src/main/java/com/resengkor/management/global/security/oauth/dto/GoogleResponse.java
@@ -0,0 +1,30 @@
+package com.resengkor.management.global.security.oauth.dto;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class GoogleResponse implements OAuth2Response {
+    private final Map<String, Object> attribute;
+
+    @Override
+    public String getSocialProvider() {
+        return "google";
+    }
+
+    @Override
+    public String getSocialId() {
+        return attribute.get("sub").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attribute.get("name").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attribute.get("email").toString();
+    }
+}
