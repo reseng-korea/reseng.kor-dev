a960ff0e849d7db1cf047cd0c97ae9b1ff8c4438
kimnayeon16 <nayeon0016@gmail.com>
2024-10-16 14:48:56 +0900

    Design: 탈퇴 페이지 UI 개발

--- FE/reseng/src/pages/mypage/Withdraw.jsx
+++ FE/reseng/src/pages/mypage/Withdraw.jsx
@@ -1,7 +1,36 @@
+import withdraw_icon from './../../assets/withdraw_icon.png';
+
+import { useNavigateTo } from '../../hooks/useNavigateTo';
+
 const Withdraw = () => {
+  // 페이지 이동
+  const { navigateTo, routes } = useNavigateTo();
+
   return (
-    <div className="flex justify-center px-3 py-2 min-h-screen mt-16">
-      <div className="w-full flex flex-col mb-1 space-x-2"></div>
+    <div className="flex flex-col items-center justify-center min-h-screen pt-16">
+      <div className="flex flex-col w-full max-w-2xl items-center p-8 text-center mx-auto">
+        <img
+          src={withdraw_icon}
+          alt="탈퇴 아이콘"
+          className="w-16 h-16 mb-12"
+        ></img>
+        <span className="text-2xl font-bold mb-6">
+          회원 탈퇴가 완료되었습니다.
+        </span>
+        <span className="text-lg mb-16">
+          그동안 Re&生을 이용해 주셔서 감사합니다.
+          <br />
+          항상 끊임없이 성장하고 발전하는 Re&生이 되겠습니다.
+        </span>
+
+        <button
+          onClick={() => navigateTo(routes.home)}
+          type="submit"
+          className="w-[25%] mb-20 bg-[#2EA642] text-white font-bold py-2 px-4 rounded-lg hover:bg-green-600"
+        >
+          확인
+        </button>
+      </div>
     </div>
   );
 };
