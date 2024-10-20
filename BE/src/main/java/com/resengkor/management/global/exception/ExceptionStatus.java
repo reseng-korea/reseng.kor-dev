0aded0d5d7d0ebb20e9906b2363467ef1c4ce6bf
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-10-20 19:40:46 +0900

    Chore:gitignore 적용

--- BE/src/main/java/com/resengkor/management/global/exception/ExceptionStatus.java
+++ BE/src/main/java/com/resengkor/management/global/exception/ExceptionStatus.java
@@ -23,6 +23,7 @@ public enum ExceptionStatus {
     AUTHENTICATION_FAILED(4003, "유저 인증에 실패하였습니다."),
     FORBIDDEN_FAILED(4003, "콘텐츠에 접근할 수 없어, 정보를 조회할 수 없습니다."),
     MEMBER_NOT_FOUND(5001, "존재하지 않는 사용자입니다."),
+    MEMBER_ALREADY_EXIST(4009, "이미 존재하는 사용자입니다."),
     DATA_NOT_FOUND(5000, "데이터가 존재하지 않습니다.");
 
     private final int code;
