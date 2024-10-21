f3bc572cbacd5a7ff3421873c82c9d00dda32ba0
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-10-21 22:28:31 +0900

    Comment:RegisterController comment

--- BE/src/main/java/com/resengkor/management/domain/user/controller/RegisterController.java
+++ BE/src/main/java/com/resengkor/management/domain/user/controller/RegisterController.java
@@ -26,11 +26,13 @@ public class RegisterController {
     // 회원가입 (일반 사용자 등록하기)
     @PostMapping("/register")
     public DataResponse<?> registerUser(@Valid @RequestBody UserRegisterRequest userRegisterRequest, BindingResult bindingResult) {
+        log.info("회원가입 요청이 들어옴: {}", userRegisterRequest);
         if(bindingResult.hasErrors()) {
             Map<String, String> validatorResult = userServiceImpl.validateHandling(bindingResult);
-
+            log.info("바인딩 에러");
             return new DataResponse<>(400, "Validation Error", validatorResult);
         }
+        log.info("회원가입 서비스로 넘어감");
         return userServiceImpl.registerUser(userRegisterRequest);
     }
 }
