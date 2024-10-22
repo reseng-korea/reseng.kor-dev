3d755844edc4d4a9b72f9880d867563826cc7044
kimnayeon16 <nayeon0016@gmail.com>
2024-10-22 14:51:34 +0900

    Design: 업체 관리 페이지 UI 개발

---
+++ FE/src/pages/data/memberData.jsx
@@ -0,0 +1,62 @@
+export const memberData = [
+  {
+    id: 1,
+    managementStatus: '관리', // 관리여부
+    companyName: 'HS한솔', // 회사
+    location: '대구광역시 중구 국채보상로 488, 8층(동산동, 섬유회관)', // 위치
+    companyPhone: '053-111-1111', // 회사번호
+    companyFax: '123456789', // 회사팩스
+    mobilePhone: '010-1111-1111', // 핸드폰번호
+    metropolitanArea: '대구광역시', // 광역자치구
+    localArea: '중구', // 지역자치구
+    userRole: '총판', // 유저 롤
+  },
+  {
+    id: 2,
+    managementStatus: '관리', // 관리여부
+    companyName: 'HS한솔', // 회사
+    location: '대구광역시 중구 국채보상로 488, 8층(동산동, 섬유회관)', // 위치
+    companyPhone: '053-111-1111', // 회사번호
+    companyFax: '123456789', // 회사팩스
+    mobilePhone: '010-1111-1111', // 핸드폰번호
+    metropolitanArea: '대구광역시', // 광역자치구
+    localArea: '중구', // 지역자치구
+    userRole: '총판', // 유저 롤
+  },
+  {
+    id: 3,
+    managementStatus: '비관리', // 관리여부
+    companyName: 'HS한솔', // 회사
+    location: '대구광역시 중구 국채보상로 488, 8층(동산동, 섬유회관)', // 위치
+    companyPhone: '053-111-1111', // 회사번호
+    companyFax: '123456789', // 회사팩스
+    mobilePhone: '010-1111-1111', // 핸드폰번호
+    metropolitanArea: '대구광역시', // 광역자치구
+    localArea: '중구', // 지역자치구
+    userRole: '총판', // 유저 롤
+  },
+  {
+    id: 4,
+    managementStatus: '비관리', // 관리여부
+    companyName: 'HS한솔', // 회사
+    location: '대구광역시 중구 국채보상로 488, 8층(동산동, 섬유회관)', // 위치
+    companyPhone: '053-111-1111', // 회사번호
+    companyFax: '123456789', // 회사팩스
+    mobilePhone: '010-1111-1111', // 핸드폰번호
+    metropolitanArea: '대구광역시', // 광역자치구
+    localArea: '중구', // 지역자치구
+    userRole: '총판', // 유저 롤
+  },
+  {
+    id: 5,
+    managementStatus: '관리', // 관리여부
+    companyName: 'HS한솔', // 회사
+    location: '대구광역시 중구 국채보상로 488, 8층(동산동, 섬유회관)', // 위치
+    companyPhone: '053-111-1111', // 회사번호
+    companyFax: '123456789', // 회사팩스
+    mobilePhone: '010-1111-1111', // 핸드폰번호
+    metropolitanArea: '대구광역시', // 광역자치구
+    localArea: '중구', // 지역자치구
+    userRole: '총판', // 유저 롤
+  },
+];
