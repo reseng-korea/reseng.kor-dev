e938717e00f7a42062ccf2fd9c102ea677fc64f5
kimnayeon16 <nayeon0016@gmail.com>
2024-10-23 19:03:06 +0900

    Design: 인증서 페이지 UI 개발

--- FE/src/pages/referenceRoom/Certificate.jsx
+++ FE/src/pages/referenceRoom/Certificate.jsx
@@ -2,6 +2,9 @@ import Layout from '../../components/Layouts';
 
 import { useNavigateTo } from '../../hooks/useNavigateTo';
 
+import tmp1 from '../../assets/tmp_1.png';
+import tmp2 from '../../assets/tmp_2.png';
+
 const Cerificate = () => {
   // 페이지 이동
   const { navigateTo, routes } = useNavigateTo();
@@ -10,6 +13,7 @@ const Cerificate = () => {
     <Layout>
       <div className="flex justify-center px-3 py-2 min-h-screen">
         <div className="w-full flex flex-col">
+          {/* 하위 카테고리 */}
           <div className="text-3xl font-bold mt-16 mb-6">자료실</div>
           <div className="flex justify-center space-x-4">
             <button
@@ -32,6 +36,90 @@ const Cerificate = () => {
             </button>
           </div>
           <hr className="w-full border-t border-gray1 mb-6" />
+          {/* 메인 */}
+          <div className="flex flex-col">
+            <div className="flex flex-wrap w-full justify-center">
+              {/* 카드 1 */}
+              <div className="flex flex-col w-full sm:w-2/5 md:w-1/4 lg:w-2/7 items-center px-12 py-8 border border-gray3 rounded-lg mx-4 my-2">
+                {/* 이미지 영역 */}
+                <div className="flex justify-center items-center h-36">
+                  <img
+                    className="max-w-full max-h-full object-contain"
+                    src={tmp1}
+                    alt="인증서 1"
+                  />
+                </div>
+                {/* 텍스트 영역 */}
+                <div className="w-full mt-4 text-center">
+                  <span className="text-lg font-bold">
+                    한국 산업의 고객만족도
+                  </span>
+                  <span className="mt-2 block">친환경 부문 10년 연속 1위</span>
+                </div>
+              </div>
+
+              {/* 카드 2 */}
+              <div className="flex flex-col w-full sm:w-2/5 md:w-1/4 lg:w-2/7 items-center px-12 py-8 border border-gray3 rounded-lg mx-4 my-2">
+                {/* 이미지 영역 */}
+                <div className="flex justify-center items-center h-36">
+                  <img
+                    className="max-w-full max-h-full object-contain"
+                    src={tmp2}
+                    alt="인증서 2"
+                  />
+                </div>
+                {/* 텍스트 영역 */}
+                <div className="w-full mt-4 text-center">
+                  <span className="text-lg font-bold">친환경 환경부</span>
+                  <span className="mt-2 block">친환경 부문 10년 연속 1위</span>
+                </div>
+              </div>
+
+              {/* 카드 3 */}
+              <div className="flex flex-col w-full sm:w-2/5 md:w-1/4 lg:w-2/7 items-center px-12 py-8 border border-gray3 rounded-lg mx-4 my-2">
+                {/* 이미지 영역 */}
+                <div className="flex justify-center items-center h-36">
+                  <img
+                    className="max-w-full max-h-full object-contain"
+                    src={tmp1}
+                    alt="인증서 3"
+                  />
+                </div>
+                {/* 텍스트 영역 */}
+                <div className="w-full mt-4 text-center">
+                  <span className="text-lg font-bold">
+                    한국 산업의 고객만족도
+                  </span>
+                  <span className="mt-2 block">친환경 부문 10년 연속 1위</span>
+                </div>
+              </div>
+              <div className="flex flex-col w-full sm:w-2/5 md:w-1/4 lg:w-2/7 items-center px-12 py-8 border border-gray3 rounded-lg mx-4 my-2">
+                {/* 이미지 영역 */}
+                <div className="flex justify-center items-center h-36">
+                  <img
+                    className="max-w-full max-h-full object-contain"
+                    src={tmp1}
+                    alt="인증서 3"
+                  />
+                </div>
+                {/* 텍스트 영역 */}
+                <div className="w-full mt-4 text-center">
+                  <span className="text-lg font-bold">
+                    한국 산업의 고객만족도
+                  </span>
+                  <span className="mt-2 block">친환경 부문 10년 연속 1위</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-end mt-12">
+              <button
+                type="submit"
+                className="px-8 py-2 font-bold text-white transition-colors duration-300 bg-primary rounded-lg hover:bg-white hover:text-primary"
+              >
+                글쓰기
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
