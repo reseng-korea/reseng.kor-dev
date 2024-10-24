c36fb4bcf9732d28a179c6a94076913fd6f70b0a
Youngin <88714716+poiu542@users.noreply.github.com>
2024-10-24 16:07:30 +0900

    Feat : Develop JwtTokenService class

--- BE/src/main/java/com/resengkor/management/domain/qrcode/service/QrPageDataService.java
+++ BE/src/main/java/com/resengkor/management/domain/qrcode/service/QrPageDataService.java
@@ -12,7 +12,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
+import java.security.Principal;
+
 
 @Service
 public class QrPageDataService {
