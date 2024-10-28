e85cb8e8923675ccfbc8ef344aaa36240563c336
Youngin <88714716+poiu542@users.noreply.github.com>
2024-10-28 17:11:36 +0900

    Feat : Develop BannerTypeRepository Interface

---
+++ BE/src/main/java/com/resengkor/management/domain/banner/repository/BannerTypeRepository.java
@@ -0,0 +1,14 @@
+package com.resengkor.management.domain.banner.repository;
+
+import com.resengkor.management.domain.banner.entity.BannerType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface BannerTypeRepository extends JpaRepository<BannerType, Long> {
+
+    @Query("SELECT bt FROM BannerType bt WHERE bt.user.id = :userId")
+    List<BannerType> findByUserId(@Param("userId") Long userId);
+}
