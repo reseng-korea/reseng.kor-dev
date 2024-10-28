0789af67fe91e420856d7873e168fcfafa7536f7
Youngin <88714716+poiu542@users.noreply.github.com>
2024-10-28 14:24:11 +0900

    Update : Modify BannerType class

--- BE/src/main/java/com/resengkor/management/domain/banner/entity/BannerType.java
+++ BE/src/main/java/com/resengkor/management/domain/banner/entity/BannerType.java
@@ -1,5 +1,6 @@
 package com.resengkor.management.domain.banner.entity;
 
+import com.resengkor.management.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,8 +14,25 @@ public class BannerType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long bannerTypeId;
+    private Long id;
 
     @Column(name = "type_width", nullable = false)
     private Integer typeWidth;
+
+    @Column(name = "horizontal_length", nullable = false)
+    private Integer horizontalLength;
+
+    @Column(name = "is_standard", nullable = false)
+    private Boolean isStandard;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_history_id", nullable = false)
+//    private OrderHistory orderHistory;
 }
