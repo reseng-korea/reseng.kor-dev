16661214c2dd57f0f18c94ecf554c57c5d785329
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-10-29 18:04:42 +0900

    Feat:SecurityConfig에 sms url 추가

--- BE/src/main/java/com/resengkor/management/global/config/SecurityConfig.java
+++ BE/src/main/java/com/resengkor/management/global/config/SecurityConfig.java
@@ -115,7 +115,7 @@ public class SecurityConfig {
                                 "/api/v1/login","/api/v1/logout",
                                 "/api/v1/oauth/**", "/api/v1/oauth2-jwt-header",
                                 "/api/v1/reissue","/api/v1/withdrawal",
-                                "/api/v1/mail/**").permitAll()
+                                "/api/v1/mail/**","/api/v1/sms/**").permitAll()
                         //hasRole() : 특정 Roll을 가져야함
                         //제일 낮은 권한을 설정해주면 알아서 높은 얘들을 허용해줌
                         //아래 roleHierarchy() 메소드 덕분
