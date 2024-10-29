d6520e13fd88eb32b45b60c655d16dbbd94eb293
Youngin <88714716+poiu542@users.noreply.github.com>
2024-10-29 11:52:35 +0900

    Update : Modify OrderHitory Entity, Add OrderStatus Enum Class

---
+++ BE/src/main/java/com/resengkor/management/domain/banner/entity/OrderStatus.java
@@ -0,0 +1,16 @@
+package com.resengkor.management.domain.banner.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum OrderStatus {
+
+    UNCONFIRMED("미확인"),
+    CONFIRMED("확인완료"),
+    SHIPPED_COURIER("출고완료(택배)"),
+    SHIPPED_FREIGHT("출고완료(화물)");
+
+    private final String description;
+}
