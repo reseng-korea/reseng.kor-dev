f01efd0edbb55b863c5405d740651e0c57c521bc
ParkSeYun98 <youn12343@gmail.com>
2024-10-30 10:30:30 +0900

    Feat: MANAGER가 전체 유저 리스트 조회

--- BE/src/main/java/com/resengkor/management/domain/user/repository/RoleHierarchyRepository.java
+++ BE/src/main/java/com/resengkor/management/domain/user/repository/RoleHierarchyRepository.java
@@ -1,17 +1,9 @@
 package com.resengkor.management.domain.user.repository;
 
 
-import com.resengkor.management.domain.user.entity.Role;
 import com.resengkor.management.domain.user.entity.RoleHierarchy;
-import com.resengkor.management.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
 
 @Repository
 public interface RoleHierarchyRepository extends JpaRepository<RoleHierarchy, Long> {
--- BE/src/main/java/com/resengkor/management/domain/user/service/AdminServiceImpl.java
+++ BE/src/main/java/com/resengkor/management/domain/user/service/AdminServiceImpl.java
@@ -1,25 +1,46 @@
 package com.resengkor.management.domain.user.service;
 
-
-import com.resengkor.management.domain.user.repository.RegionRepository;
+import com.resengkor.management.domain.user.entity.Role;
+import com.resengkor.management.domain.user.entity.User;
 import com.resengkor.management.domain.user.repository.RoleHierarchyRepository;
-import com.resengkor.management.domain.user.repository.UserProfileRepository;
 import com.resengkor.management.domain.user.repository.UserRepository;
-import com.resengkor.management.domain.user.dto.UserDTO;
-import com.resengkor.management.domain.user.dto.UserMapper;
-import com.resengkor.management.domain.user.dto.UserRegisterRequest;
-import com.resengkor.management.domain.user.entity.*;
-import jakarta.transaction.Transactional;
+import com.resengkor.management.global.exception.CustomException;
+import com.resengkor.management.global.exception.ExceptionStatus;
+import com.resengkor.management.global.response.DataResponse;
+import com.resengkor.management.global.response.ResponseStatus;
+import com.resengkor.management.global.security.authorization.UserAuthorizationUtil;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-
 @Service
+@Getter
 @RequiredArgsConstructor
 @Slf4j
+@Transactional(readOnly = true)
 public class AdminServiceImpl {
 
+    private final UserRepository userRepository;
+    private final RoleHierarchyRepository roleHierarchyRepository;
+
+    @Transactional
+    public DataResponse<List<User>> getAllUserByManager() {
+
+        Long userId = UserAuthorizationUtil.getLoginMemberId();
+
+        User loginUser = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ExceptionStatus.MEMBER_NOT_FOUND));
+
+        if (loginUser.getRole() != Role.ROLE_MANAGER) {
+            // 예외 처리
+        }
+
+        List<User> userList = userRepository.findAll();
+
+        return new DataResponse<>(ResponseStatus.RESPONSE_SUCCESS.getCode(), ResponseStatus.RESPONSE_SUCCESS.getMessage(), userList);
+    }
 }
