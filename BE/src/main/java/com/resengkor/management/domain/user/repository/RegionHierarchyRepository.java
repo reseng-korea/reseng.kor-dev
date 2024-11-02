33c0f9d5ddbae1b88a07d7e2bef91dad4a8da294
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-11-02 17:44:12 +0900

    Feat:지역 관련 api 구현 1.RegionController에 City카테고리 가져오는 API 구현/특정 CITY에 해당하는 DISTRICT를 가져오는 API 구현 2. RegionDTO 생성 3. RegionService 구현 4. SecurityConfig에 endpoint 추가

---
+++ BE/src/main/java/com/resengkor/management/domain/user/repository/RegionHierarchyRepository.java
@@ -0,0 +1,14 @@
+package com.resengkor.management.domain.user.repository;
+
+import com.resengkor.management.domain.user.entity.Region;
+import com.resengkor.management.domain.user.entity.RegionHierarchy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RegionHierarchyRepository extends JpaRepository<RegionHierarchy, Long> {
+    @Query("SELECT rh.descendantRegion FROM RegionHierarchy rh WHERE rh.ancestorRegion = :ancestor")
+    List<Region> findDescendantsByAncestor(@Param("ancestor") Region ancestor);
+}
