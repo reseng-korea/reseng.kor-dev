1b8c2ae0bc7cdfefac69b5eba44a97db44f89448
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-11-08 20:55:37 +0900

    feat: add file size validation to uploadFileToS3 method

--- BE/src/main/java/com/resengkor/management/domain/qualification/service/QualificationService.java
+++ BE/src/main/java/com/resengkor/management/domain/qualification/service/QualificationService.java
@@ -12,6 +12,7 @@ import com.resengkor.management.global.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
