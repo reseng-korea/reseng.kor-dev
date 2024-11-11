6dee7c4f3d6ca81a2d8e395e052483cf0426e289
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-11-11 13:54:30 +0900

    fix:valid error 처리 수정

--- BE/src/main/java/com/resengkor/management/global/util/TmpCodeUtil.java
+++ BE/src/main/java/com/resengkor/management/global/util/TmpCodeUtil.java
@@ -22,18 +22,21 @@ public class TmpCodeUtil {
     public static String generateAlphanumericPasswordWithSpecialChars() {
         Random random = new Random();
         StringBuilder key = new StringBuilder();
-        String specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?"; // 특수문자 리스트
+        String specialChars = "!@#$%^&*"; // 특수문자 리스트
 
-        for (int i = 0; i < 8; i++) { // 인증 코드 8자리
-            int index = random.nextInt(4); // 0~3까지 랜덤, 랜덤값으로 switch문 실행
+        for (int i = 0; i < 7; i++) { // 앞의 7자리는 소문자, 대문자, 숫자로 구성
+            int index = random.nextInt(3); // 0~2까지 랜덤
 
             switch (index) {
                 case 0 -> key.append((char) (random.nextInt(26) + 97)); // 소문자
                 case 1 -> key.append((char) (random.nextInt(26) + 65)); // 대문자
                 case 2 -> key.append(random.nextInt(10)); // 숫자
-                case 3 -> key.append(specialChars.charAt(random.nextInt(specialChars.length()))); // 특수문자
             }
         }
+
+        // 마지막 글자는 특수문자로 설정
+        key.append(specialChars.charAt(random.nextInt(specialChars.length())));
+
         return key.toString();
     }
 
