95e3c6de3e22674f854c583e9a7b1282d16ff42b
Youngin <88714716+poiu542@users.noreply.github.com>
2024-11-14 01:00:28 +0900

    Update : Modify Security config class

--- BE/src/main/java/com/resengkor/management/global/config/SecurityConfig.java
+++ BE/src/main/java/com/resengkor/management/global/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
             "/api/v1/users/pagination",
             "/api/v1/regions/**", "/api/v1/companies/**",
             "/api/v1/faq/**",
-            "api/v1/qr-code"
+            "api/v1/qr-code",
             "/api/v1/qna/questions/**",
             "/api/v1/qualifications"
     );
