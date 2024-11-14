0d3c397656ab71898a7535c70d7e0dce8dbbd661
kimnayeon16 <nayeon0016@gmail.com>
2024-11-15 01:59:52 +0900

    Fix: 카카오 로그인 api 빈 객체 생성

--- FE/src/components/OAuthRedirectHandler.jsx
+++ FE/src/components/OAuthRedirectHandler.jsx
@@ -15,11 +15,9 @@ function OAuthRedirectHandler() {
         // 로그인 상태를 확인하는 API 호출 (쿠키 기반 인증)
         const response = await axios.post(
           `${apiUrl}/api/v1/oauth2-jwt-header`,
+          {},
           {
-            withCredentials: true,
-            // headers: {
-            //   Refresh: 'your-refresh-token', // 쿠키에서 자동으로 포함
-            // }, // 쿠키 포함
+            withCredentials: true, // 쿠키를 포함하도록 설정
           }
         );
 
