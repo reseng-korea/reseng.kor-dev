f71da34906ae27dcbf4e0aaa7c09c0b894e09ed6
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-11-20 01:12:21 +0900

    Fix : 이메일 비동기 처리 수정

--- BE/src/main/java/com/resengkor/management/global/config/AsyncConfig.java
+++ BE/src/main/java/com/resengkor/management/global/config/AsyncConfig.java
@@ -15,15 +15,20 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Configuration
 @EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
+    private static final int CORE_POOL_SIZE = 10;
+    private static final int MAX_POOL_SIZE = 50;
+    private static final int QUEUE_CAPACITY = 20;
+
     @Bean(name = "emailAsyncExecutor")
     public Executor getAsyncExecutor() {
         final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(25);
-        executor.setQueueCapacity(30);
+        executor.setCorePoolSize(CORE_POOL_SIZE); //기본 스레드 수
+        executor.setMaxPoolSize(MAX_POOL_SIZE); //최대 스레드 수
+        executor.setQueueCapacity(QUEUE_CAPACITY);
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.setAwaitTerminationSeconds(60);
-        executor.setThreadNamePrefix("Asynchronous Mail Sender Thread-");
+        executor.setThreadNamePrefix("Asynchronous Mail Sender Thread-"); //스레드 이름
+        executor.initialize();
         return executor;
     }
 
