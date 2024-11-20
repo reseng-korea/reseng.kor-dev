68027460f4c0f132d6dc2b49200496676b6ad767
kimnayeon16 <nayeon0016@gmail.com>
2024-11-20 20:20:57 +0900

    Feat: 에디터 사진 등록 시 api 연결

--- FE/src/pages/Tmp.jsx
+++ FE/src/pages/Tmp.jsx
@@ -50,32 +50,13 @@ const Tmp = () => {
   return (
     <div className="flex justify-center px-3 py-2 min-h-screen mt-16">
       <div className="w-full flex flex-col mb-1 space-x-2">
-        <button onClick={() => navigateTo(routes.socialinfo)}>
-          간편 로그인 시 추가정보 입력 페이지
-        </button>
-        <button onClick={() => navigateTo(routes.idinquirySuccess)}>
-          아이디 찾기 성공 페이지
-        </button>
-        <button onClick={() => navigateTo(routes.idinquiryFailure)}>
-          아이디 찾기 실패 페이지
-        </button>
-        <button onClick={() => navigateTo(routes.pwinquiryNew)}>
-          비밀번호 변경 페이지
-        </button>
         <button onClick={() => navigateTo(routes.itemsBanner)}>
           아이템 페이지
         </button>
         <button onClick={() => navigateTo(routes.mypageMember)}>
           마이페이지
         </button>
-        <button onClick={() => navigateTo(routes.mypageWithdraw)}>
-          탈퇴페이지
-        </button>
-        <button onClick={() => navigateTo(routes.qnaDetail)}>
-          1:1 문의 상세 페이지
-        </button>
-        <button onClick={() => navigateTo(routes.qrSuccess)}>qr 성공</button>
-        <button onClick={() => navigateTo(routes.qrFailure)}>qr 실패</button>
+
         <button onClick={handleLogout}>로그아웃</button>
       </div>
       {modalOpen && <RenderModal />}
