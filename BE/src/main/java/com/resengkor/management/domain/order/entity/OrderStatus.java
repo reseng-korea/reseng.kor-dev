9aec739558df883565854fb6c5417914ff470402
Youngin <88714716+poiu542@users.noreply.github.com>
2024-11-27 16:02:52 +0900

    Update : add Validation Process for Delivery and Order Status Changes

--- BE/src/main/java/com/resengkor/management/domain/order/entity/OrderStatus.java
+++ BE/src/main/java/com/resengkor/management/domain/order/entity/OrderStatus.java
@@ -1,7 +1,10 @@
 package com.resengkor.management.domain.order.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Getter
@@ -13,4 +16,17 @@ public enum OrderStatus {
     SHIPPED_FREIGHT("출고완료(화물)");
 
     private final String description;
+    private List<OrderStatus> allowedNextStatuses;
+
+    // 초기화 블록에서 상태 전이 정의
+    static {
+        UNCONFIRMED.allowedNextStatuses = List.of(CONFIRMED);
+        CONFIRMED.allowedNextStatuses = List.of(SHIPPED_COURIER, SHIPPED_FREIGHT);
+        SHIPPED_COURIER.allowedNextStatuses = List.of();
+        SHIPPED_FREIGHT.allowedNextStatuses = List.of();
+    }
+
+    public boolean canTransitionTo(OrderStatus nextStatus) {
+        return allowedNextStatuses.contains(nextStatus);
+    }
 }
