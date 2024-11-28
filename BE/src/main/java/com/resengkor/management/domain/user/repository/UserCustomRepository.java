98b75fd74835993cbc345378bb852024648015b3
ParkSeYun98 <youn12343@gmail.com>
2024-11-28 17:03:09 +0900

    Fix: 회원가입 시 관리자-새 유저 관계 추가

--- BE/src/main/java/com/resengkor/management/domain/user/repository/UserCustomRepository.java
+++ BE/src/main/java/com/resengkor/management/domain/user/repository/UserCustomRepository.java
@@ -2,13 +2,18 @@ package com.resengkor.management.domain.user.repository;
 
 import com.resengkor.management.domain.user.dto.UserListPaginationDTO;
 import com.resengkor.management.domain.user.entity.Role;
+import com.resengkor.management.domain.user.entity.User;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserCustomRepository {
+
+    // Manager (관리자) 찾기
+    Optional<User> findManagerUser();
 
     UserListPaginationDTO getAllUserByManager(Pageable pageable, String role, String status, LocalDateTime createdDate, List<Role> accessibleRoles, String companyName, String city, String district);
 }
