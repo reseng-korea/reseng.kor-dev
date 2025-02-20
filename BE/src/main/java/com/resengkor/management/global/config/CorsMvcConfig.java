1e1214c702a7da1337e96b78f9e1925899a0ab88
howkkw <82790801+howkkw@users.noreply.github.com>
2025-02-20 11:35:40 +0900

    쿠키 저장 제거

--- BE/src/main/java/com/resengkor/management/global/config/CorsMvcConfig.java
+++ BE/src/main/java/com/resengkor/management/global/config/CorsMvcConfig.java
@@ -9,7 +9,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .exposedHeaders("Set-Cookie") //노출할 헤더값은 쿠키 헤더
                 .allowedOrigins("http://localhost:5173","https://reseng.co.kr"); //웹앱이 동작할 서버 주소
     }
 }
