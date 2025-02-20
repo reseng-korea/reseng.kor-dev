24450f38dea246926bc800dfaa120aba4286103d
howkkw <82790801+howkkw@users.noreply.github.com>
2025-02-20 10:49:25 +0900

    cookie test2

--- BE/src/main/java/com/resengkor/management/global/security/jwt/filter/CustomLoginFilter.java
+++ BE/src/main/java/com/resengkor/management/global/security/jwt/filter/CustomLoginFilter.java
@@ -130,7 +130,7 @@ public class CustomLoginFilter extends UsernamePasswordAuthenticationFilter {
         log.info("Refresh 토큰 Redis 저장 성공");
 
         // 4. Access Token을 헤더에 추가 (프론트에서 API 요청에 사용)
-        response.setHeader("Authorization", "Bearer " + accessToken);
+        //response.setHeader("Authorization", "Bearer " + accessToken);
 
         // 5. 응답 JSON 반환 (Refresh Token은 프론트로 절대 보내지 않음)
         LoginResponse loginResponse = LoginResponse.builder()
