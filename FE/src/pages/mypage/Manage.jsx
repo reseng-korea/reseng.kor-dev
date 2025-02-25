5b1d7b1c1d8005a4a0ba9992d7feead74bca3b4b
howkkw <82790801+howkkw@users.noreply.github.com>
2025-02-25 21:08:36 +0900

    Update Manage.jsx

--- FE/src/pages/mypage/Manage.jsx
+++ FE/src/pages/mypage/Manage.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 import apiClient from '../../services/apiClient';
-
+import { useUserInfo } from '../../hooks/userContext'; 
 import Layout from '../../components/Layouts';
 import SubNavbar from '../../components/SubNavbar';
 
 import { useNavigateTo } from '../../hooks/useNavigateTo';
 
 const Manage = () => {
+  const { userInfo } = useUserInfo();
   const apiUrl = import.meta.env.VITE_API_BASE_URL;
   const loginType = userInfo.loginType;
 
