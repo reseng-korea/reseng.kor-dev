0666c01292dc05e4722ce67e10860756b170e26a
howkkw <82790801+howkkw@users.noreply.github.com>
2025-02-25 20:45:06 +0900

    경로수정

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { useUserInfo } from '.hooks/userContext';
+import { useUserInfo } from './hooks/userContext';
 import {
   BrowserRouter as Router,
   Routes,
