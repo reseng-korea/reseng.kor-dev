fa3acf25908a87fe1d37629d080245cdb2c85657
howkkw <82790801+howkkw@users.noreply.github.com>
2025-02-25 15:27:44 +0900

    로그인부분수정 3

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -168,7 +168,7 @@ function App() {
   return (
     <ModalProvider>
       <ScrollToTop />
-      {!shouldHideNavbar && <Navbar userInfo={userInfo} isLoggedIn={isLoggedIn}/>}
+      {!shouldHideNavbar && <Navbar userInfo={userInfo} isLoggedIn={isAuthenticated()}/>}
       <div className="h-screen">
         <Routes>
           {/* 메인페이지 */}
