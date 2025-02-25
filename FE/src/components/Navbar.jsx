54280346684d8c8a99c0649e1176c40318dc99a8
howkkw <82790801+howkkw@users.noreply.github.com>
2025-02-25 15:11:08 +0900

    Update Navbar.jsx

--- FE/src/components/Navbar.jsx
+++ FE/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-export default function Example() {
+export default function Example({userInfo}) {
   // 페이지 이동
   const { navigateTo, routes } = useNavigateTo();
 
