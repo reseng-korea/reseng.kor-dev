2c35f6222319d392e9187b6763f98ceadb457fa8
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-03-20 11:22:51 +0900

    Fix:fix AsyncConfig

--- BE/src/main/java/com/resengkor/management/global/config/AsyncConfig.java
+++ BE/src/main/java/com/resengkor/management/global/config/AsyncConfig.java
@@ -16,8 +16,8 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
     private static final int CORE_POOL_SIZE = 10;
-    private static final int MAX_POOL_SIZE = 50;
-    private static final int QUEUE_CAPACITY = 20;
+    private static final int MAX_POOL_SIZE = 20;
+    private static final int QUEUE_CAPACITY = 50;
 
     @Bean(name = "emailAsyncExecutor")
     public Executor getAsyncExecutor() {
